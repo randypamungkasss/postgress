34e44a74ded579a60530f5fb2b3edb5a0c68f1d9
Randy <anakkantorku@gmail.com>
2025-04-01 21:07:06 +0700

    Import IndexView and replace index_view(IndexView) in /todos/urls.py

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index_view, detail_view
+from .views import index_view, detail_view, IndexView
 
 urlpatterns = [
-    path('', index_view, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('todos/<str:id>/', detail_view, name='detail-view')
 ]
