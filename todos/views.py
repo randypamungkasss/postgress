129ee1a954f23b74e38244274ae79a87165719a1
Randy <anakkantorku@gmail.com>
2025-04-01 21:04:46 +0700

    Create class IndexView in /todos/views.py

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from django.views import View
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        todos = Todo.objects.all()
+        return render(request, 'index.html', {"todos": todos})
+    
+    def post(self,request):
+        title = request.POST.get('title')
+        task = request.POST.get('task')
+        print(f'title: {title}, task: {task}')
+        return redirect('index')
+
 def index_view(request):
     todos = Todo.objects.all()
     return render(request, 'index.html', {"todos": todos})
