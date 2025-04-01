cde88a9a65a5681b9c24a208d11f8f7bd9d77f8f
Randy <anakkantorku@gmail.com>
2025-04-02 01:12:16 +0700

    Create Path DeleteView in /todos/urls.py

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, DetailView
+from .views import IndexView, DetailView, DeleteView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('todos/<str:id>/', DetailView.as_view(), name='detail-view')
+    path('todos/<str:id>/', DetailView.as_view(), name='detail-view'),
+    path('todos/<str:id>/delete/', DeleteView.as_view(), name='delete')
 ]
